a9d25b2d842ca99cb63d3be5ec9b8f4692b904d0
Vladimir Rujević <vladimir.rujevic@live.com>
2025-03-24 17:31:21 +0100

    Static site generator boilerplate - add TextNode class and TextType enum - add main.py - runs a test - remove public folder - add __pycache__ to .gitignore

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#! /bin/env python3
+from textnode import TextNode, TextType
+
+def main():
+    test_node = TextNode("This is a test", TextType.LINK.value, "https://google.com")
+    print(test_node)
+
+if __name__ == '__main__':
+    main()

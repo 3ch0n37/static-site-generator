df3482d258ac6874351374deefd99c7d0c620210
Vladimir Rujević <vladimir.rujevic@live.com>
2025-03-28 18:55:42 +0100

    Add static site generator enhancements and migrate content to docs
    Refactored the site generator to improve flexibility, including support for customizable base paths for static assets. Migrated public outputs to the docs directory to align with modern deployment practices.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 #! /bin/env python3
 import os
 import shutil
+import sys
 from pathlib import Path
+
+from mercurial.wireprotoframing import outputstream
+
 from site_generator import generate_page, generate_pages_recursive
 
 def recursive_copy(src, dest):
@@ -16,25 +20,28 @@ def recursive_copy(src, dest):
             print(f"copying file {s} to {d}")
             shutil.copy(s, d)
 
-def main():
+def main(basepath = "/"):
     path = Path(__file__)
     root_dir = path.parent.parent.absolute()
     print(root_dir)
-    public_dir = os.path.join(root_dir, 'public')
+    output_dir = os.path.join(root_dir, 'docs')
     static_dir = os.path.join(root_dir, 'static')
-    if os.path.exists(public_dir):
-        shutil.rmtree(public_dir, ignore_errors=True)
-    os.mkdir(public_dir)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir, ignore_errors=True)
+    os.mkdir(output_dir)
 
-    recursive_copy(static_dir, public_dir)
+    recursive_copy(static_dir, output_dir)
 
     template_file = os.path.join(root_dir, 'template.html')
     if not os.path.exists(template_file):
         raise ValueError(f"Template file does not exist")
     content_dir = os.path.join(root_dir, 'content')
 
-    generate_pages_recursive(content_dir, template_file, public_dir)
+    generate_pages_recursive(content_dir, template_file, output_dir, basepath)
 
 
 if __name__ == '__main__':
-    main()
+    basepath = "/"
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    main(basepath)
--- src/site_generator.py
+++ src/site_generator.py
@@ -7,7 +7,7 @@ def extract_title(markdown):
         raise Exception('Markdown file must have a title')
     return sections[0].replace('# ', '')
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath = "/"):
     with open(from_path, 'r') as f:
         markdown = f.read()
     title = extract_title(markdown)
@@ -18,18 +18,19 @@ def generate_page(from_path, template_path, dest_path):
     with open(template_path, 'r') as f:
         template = f.read()
     page = template.replace('{{ Title }}', title).replace('{{ Content }}', html_content)
+    page = page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     with open(dest_path, 'w') as f:
         f.write(page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath = "/"):
     for item in os.listdir(dir_path_content):
         path = os.path.join(dir_path_content, item)
         if os.path.isdir(path):
             new_dir = os.path.join(dest_dir_path, item)
             if not os.path.exists(new_dir):
                 os.mkdir(new_dir)
-            generate_pages_recursive(path, template_path, new_dir)
+            generate_pages_recursive(path, template_path, new_dir, basepath)
         else:
             if item.endswith('.md'):
                 dest_path = os.path.join(dest_dir_path, item.replace('.md', '.html'))
-                generate_page(path, template_path, dest_path)
+                generate_page(path, template_path, dest_path, basepath)

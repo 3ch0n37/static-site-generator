9103a5c344e7017be4e589103d4212a2eb5403d9
Vladimir Rujević <vladimir.rujevic@live.com>
2025-03-28 17:38:13 +0100

    Add static resources and implement recursive directory copying
    Introduced a `recursive_copy` function to handle directory and file copying. Added initial static assets, including `index.css` for styling and an image file. Updated `.gitignore` to exclude the `public` directory and other development-specific files.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
 #! /bin/env python3
+import os
+import shutil
+from pathlib import Path
 from textnode import TextNode, TextType
 
+def recursive_copy(src, dest):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dest, item)
+        if os.path.isdir(s):
+            print(f"copying directory {s} to {d}")
+            os.mkdir(d)
+            recursive_copy(s, d)
+        else:
+            print(f"copying file {s} to {d}")
+            shutil.copy(s, d)
+
 def main():
+    path = Path(__file__)
+    root_dir = path.parent.parent.absolute()
+    print(root_dir)
+    public_dir = os.path.join(root_dir, 'public')
+    static_dir = os.path.join(root_dir, 'static')
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir, ignore_errors=True)
+    os.mkdir(public_dir)
+
+    recursive_copy(static_dir, public_dir)
+
     test_node = TextNode("This is a test", TextType.LINK, "https://google.com")
     print(test_node)
 
